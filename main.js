8acfb2d1190dde1128eef82f2ee85a97c89b4aaa
Atiqur Rahman <39456914+atiq275217@users.noreply.github.com>
2019-10-28 16:08:35 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//var
+var Masum="billah";
+var Alamin="Islam";
+console.log(Masum);
+console.log(Alamin);
+var myName="PK";
+var myAge="25";
+console.log("Atiqur Rahman",myName,"and my age is",myAge,"years");
+//constant
+const pi = 3.14159;
+console.log("value of pi is",pi);
+
+/* End part 1 BY lear with
+Hasin Hayder*/
